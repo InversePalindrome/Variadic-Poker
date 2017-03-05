32540d4307f1f30dc561aeae8e819795d8b2c184
BryanTriana <bryantrianarueda@gmail.com>
2017-03-05 15:35:26 -0500

    Create Card.hpp

---
+++ include/Card.hpp
@@ -0,0 +1,37 @@
+/*
+Copyright (c) 2017 InversePalindrome
+VariadicPoker - Card.hpp
+InversePalindrome.com
+*/
+
+
+#pragma once
+#include <string>
+
+
+class Card
+{
+public:
+	enum Suit { Spade, Club, Diamond, Heart };
+	enum Rank { Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King, Ace };
+
+	Card(Suit suit, Rank rank);
+
+	Suit getSuit() const;
+	Rank getRank() const;
+
+	void setSuit(Suit suit);
+	void setRank(Rank rank);
+
+	std::string toString() const;
+
+	bool operator==(const Card& otherCard) const;
+	bool operator!=(const Card& otherCard) const;
+	bool operator<(const Card& otherCard) const;
+	bool operator>(const Card& otherCard) const;
+
+private:
+	Suit suit;
+	Rank rank;
+};
+

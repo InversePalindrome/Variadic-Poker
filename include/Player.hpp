dbf95d3925b7ad7b1b7aed9fb17ee583bbd5a9ea
BryanTriana <bryantrianarueda@gmail.com>
2017-03-11 19:56:18 -0500

    Added data member holecards and playingstatus

--- include/Player.hpp
+++ include/Player.hpp
@@ -15,24 +15,34 @@ InversePalindrome.com
 class Player
 {
 public:
-	Player(const std::string& name, double stack);
+	Player(const std::string& name, std::size_t stack);
 	Player(const std::string& name);
 
 	std::string getName() const;
-	double getStack() const;
+	std::size_t getStack() const;
 	HoleCards getHoleCards() const;
 
 	void setName(const std::string& name);
-	void setStack(double stack);
+	void setStack(std::size_t stack);
 	void setHoleCards(const HoleCards& holeCards);
+	void setPlayingStatus(bool playingStatus);
 
-	void addToStack(double chips);
-	void removeFromStack(double chips);
+	void addToStack(std::size_t chips);
+	void removeFromStack(std::size_t chips);
+	void removeAllStack();
+	void removeHoleCards();
+
+	bool isPlaying() const;
 
 	std::string toString() const;
 
+	bool operator==(const Player& otherPlayer) const;
+	bool operator!=(const Player& otherPlayer) const;
+
 private:
 	std::string name;
-	double stack;
+	std::size_t stack;
 	HoleCards holeCards;
+	bool playingStatus;
 };
+

91be29d6458c2828b9c29cc4c827084e5a849922
BryanTriana <bryantrianarueda@gmail.com>
2017-03-11 19:52:55 -0500

    Added more utility methods

--- include/Deck.hpp
+++ include/Deck.hpp
@@ -20,14 +20,19 @@ public:
 
 	std::vector<Card> getDeck() const;
 	std::size_t getSize() const;
-	Card dealCard();
+	std::vector<Card> getCards(std::size_t numOfCards) const;
+	Card getCard() const;
 
 	void setDeck(const std::vector<Card>& deck);
 
 	void shuffle();
-	void removeCard();
 	void removeCard(const Card& card);
+	void removeCards(std::size_t numOfCards);
+	void clearDeck();
 	void addCard(const Card& card);
+	void addCards(const std::vector<Card>& cards);
+
+	bool hasCards() const;
 
 private:
 	std::vector<Card> deck;
